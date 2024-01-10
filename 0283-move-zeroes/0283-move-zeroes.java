61da8883cc2877bb2e76d776c1620d5ddcfea848
Phalgunigithub <97692467+Phalgunigithub@users.noreply.github.com>
2024-01-10 20:17:41 +0530

    Time: 2 ms (75.37%), Space: 45.9 MB (27.37%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,7 +1,7 @@
 //same as remove duplicate
 //but in remove duplicate  we only print till elemnts we want here want to also prit  0 so use while loop to print that
 //remaining positions in the array (from insertPos onwards) are filled with zeros using a while loop.
-
+//while printing array not till j but arr.length
 
 //if     nums[++j]=nums[i];  wrong
 //o/p    Your input
@@ -31,3 +31,22 @@ class Solution {
         
     }
 }
+
+/*int[] arr= {0,3,4,0,6,0};
+		
+		int j=0;
+		
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]!=0) {
+				arr[j++]=arr[i];
+			}
+		}
+		
+		while(j<arr.length) {
+			arr[j++]=0;
+		}
+		
+		
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}*/
