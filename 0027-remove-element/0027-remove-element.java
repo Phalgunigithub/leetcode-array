de8c01e3ceac787fa6904349700ab9989bd15223
Phalgunigithub <97692467+Phalgunigithub@users.noreply.github.com>
2024-01-10 19:53:59 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (27.27%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,3 +1,9 @@
+//same as remove duplicate
+//but  if(nums[i]!=val){
+//     not ++j but j++ or else index out of bound exception
+//       return j; not j+1
+//
+        
 class Solution {
     public int removeElement(int[] nums, int val) {
         
@@ -12,3 +18,18 @@ class Solution {
         
     }
 }
+
+/*int[] arr= {1,2,3,3,4,5};
+		
+		int val=sc.nextInt();
+		
+		int j=0;
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]!=val) {
+				arr[j++]=arr[i];
+			}
+		}
+		
+		for(int i=0;i<j;i++) {
+			System.out.print(arr[i]+" ");
+		}*/
