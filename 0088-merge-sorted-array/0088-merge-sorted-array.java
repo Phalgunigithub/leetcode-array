e4ba91517efb3a4dd9dbfd64512f2c6f54b720ff
Phalgunigithub <97692467+Phalgunigithub@users.noreply.github.com>
2024-02-10 13:21:54 +0530

    Time: 0 ms (100.00%), Space: 42 MB (57.87%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,11 +1,36 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+     //approach 1 
+    //1)for loop for nums array from j=0 to n
+    //2) also initialize i=m(so that we add from second array afted  //valid first array elem)
+    //3)put elem form num2 to num1
+    //4)go to nexrt ith elem (i++)
+    //5)now as all elem added sort num1
         
-        for(int j=0,i=m;j<n;j++){
+        /*for(int j=0,i=m;j<n;j++){
             nums1[i]=nums2[j];
             i++;
         }
         Arrays.sort(nums1);
         
+    }*/
+        
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
+        
+        while(j>=0){
+            if(i>=0 && nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                k--;
+                i--;
+            }
+            else{
+                nums1[k]=nums2[j];
+                k--;
+                j--;
+            }
+        }
     }
+        
 }
